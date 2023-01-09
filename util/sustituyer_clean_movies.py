90cc791164a0d0df88c6369575acfbf4e4055d18
Josu Gomez <josugp@gmail.com>
2023-01-09 17:05:43 +0100

    arreglos movies_clean

--- util/sustituyer_clean_movies.py
+++ util/sustituyer_clean_movies.py
@@ -7,6 +7,6 @@ import re
 with open("../data/movies.txt", "r") as movies:
     for movie in movies:
         movievalues = movie.split("\t")
-        nuevamovie = procesafrase(movie[0].strip())
+        nuevamovie = procesafrase(movievalues[0].strip())
         if not(movievalues[0].strip().lower() == nuevamovie.lower()):
-            print(movievalues[0].strip() +"\t"+ movievalues[1].strip() +"\t"+ movievalues[2].strip())
+            print(nuevamovie +"\t"+ movievalues[1].strip() +"\t"+ movievalues[2].strip())
